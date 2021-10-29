0ad71624b7567370ac6c05f2b5e8dd6fd189744c
Vijay pratap singh parihar <80000851+Vijay-Pratap-Singh-Parihar@users.noreply.github.com>
2021-10-29 17:23:42 +0530

    Add files via upload

---
+++ src/components/PostList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {PostDetail} from './PostDetail';
+import { useSelector } from 'react-redux';
+
+
+export const PostList = () => {
+    const { posts } = useSelector(state => state)
+    return (
+        <div className="container">
+            <div className="row">
+                {posts &&
+                    posts.map((post) => <PostDetail post={post} key={post.id }/>)}
+            </div>
+        </div>
+    );
+}
---
+++ src/store/reducers/postReducer.js
@@ -0,0 +1,22 @@
+const initState = {
+    posts: [],
+};
+
+const postReducer = (state = initState, action)=> {
+    switch(action.type){
+        case 'DELETE_POST':
+            const newPost = state.posts.filter((post) => post.id !== action.id);
+            return {
+                posts: newPost,
+            };
+        case 'ADD_POST':
+            return {
+                // posts: [...state.posts, action.post],
+                posts: [action.post, ...state.posts],
+            }
+        default:
+            return state;
+    }
+};
+
+export default postReducer;
